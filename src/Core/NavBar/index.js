7484835de211afac35813add50d62970b353dd6c
David <ramplingd@gmail.com>
2021-11-09 21:53:57 +0000

    feat: add basic NavBar to Core

--- src/Core/NavBar/index.js
+++ src/Core/NavBar/index.js
@@ -1,1 +1,13 @@
-export const NavBar = () => "Navbar";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export const NavBar = () => (
+  <AppBar position="static">
+    <Toolbar variant="dense">
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        ERT Notes
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
